0f8f10e0a3a89a7c30fd89ab8b6446ebd9a22f65
tamicasireim <msimatic.utc@gmail.com>
2015-05-31 17:08:49 +0200

    debut de l'implémentation de l'ajout de programmation (pas encore fonctionnel =/)

---
+++ Application/Classe/evenement.cpp
@@ -0,0 +1,5 @@
+#include "evenement.h"
+
+Evenement::Evenement()
+{
+}
---
+++ Application/Classe/evenement.h
@@ -0,0 +1,10 @@
+#ifndef EVENEMENT_H
+#define EVENEMENT_H
+
+class Evenement
+{
+public:
+   Evenement();
+};
+
+#endif // EVENEMENT_H
